de3be9c6782ab4ed25ded346b68cc31856ba943a
iMearner <201452060@iiitvadodara.ac.in>
2017-09-07 15:27:54 +0530

    movie details are added

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,2 +1,50 @@
-# this file contains detais about movies 
-# hello changes 
+import main_media_player
+import media 
+
+def main():
+	# setting values of the images in their object 
+	martian = media.Movie("The Martian",
+						  "A man is stuck on Mars",			
+						   "https://upload.wikimedia.org/wikipedia/en/c/cd/"
+						   "The_Martian_film_poster.jpg",
+						   "https://www.youtube.com/watch?v=lQqhfq87FgY",
+						   "October 2, 2015")
+
+	contact = media.Movie("Contact",
+                          "We make contact with aliens",
+                          "https://upload.wikimedia.org/wikipedia/en/7/75/Contact_ver2.jpg",
+                          "https://www.youtube.com/watch?v=d9C2cF3KvP8",
+                          "July 11, 1997")
+
+    res_evil = media.Movie("Resident Evil",
+                           "Zombie-causing virus escapes from the lab",
+                           "https://upload.wikimedia.org/wikipedia/en/a/a1/Resident_evil_ver4.jpg",
+                           "https://www.youtube.com/watch?v=u6uDnd_v5Bw",
+                           "March 15, 2002")
+
+    matrix = media.Movie("The Matrix",
+                         "The world is a simulation",
+                         "https://upload.wikimedia.org/wikipedia/en/c/c1/The_Matrix_Poster.jpg",
+                         "https://www.youtube.com/watch?v=vKQi3bBA1y8",
+                         "March 31, 1999")
+
+    the_dish = media.Movie("The Dish",
+                           "A film about a radio telescope",
+                           "https://upload.wikimedia.org/wikipedia/en/4/4a/Thedish_poster.jpg",
+                           "https://www.youtube.com/watch?v=2TAqXENo1rA",
+                           "October 19, 2000")
+
+    spectre = media.Movie("Spectre",
+                          "The latest James Bond movie",
+                          "https://upload.wikimedia.org/wikipedia/en/c/c3/Spectre_poster.jpg",
+                          "https://www.youtube.com/watch?v=vBnGxAkdh_k",
+                          "October 26, 2015")
+
+    # store the movie objects in the list 
+    movies = [martian , contact , res_evil , matrix , the_dish , spectre]
+
+    main_media_player.open_movies_page(movies) ;
+
+
+if __name__ == '__main__':
+	main()
